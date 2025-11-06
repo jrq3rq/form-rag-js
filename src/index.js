37058f331de4900c092d8851e4ce01f16a7d3c38
James Santos <jamesrrs.me@gmail.com>
2025-11-06 14:29:10 -0800

    fix: force .jsx → .js in ESM output with module-resolver

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // src/index.js
-import FormRAG from "./components/FormRAG.js";
+import FormRAG from "./components/FormRAG.jsx";
 import { constructPrompt } from "./lib/promptEngine.js";
-import { UniversalSMBTemplate } from "../../templates/universal-smb.js";
+import { UniversalSMBTemplate } from "./templates/universal-smb.js";
 
 export { FormRAG, constructPrompt, UniversalSMBTemplate };
