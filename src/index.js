e888b3d7fa837f46505697390aa46984db307735
James Santos <jamesrrs.me@gmail.com>
2025-11-06 13:36:57 -0800

    fix: correct relative path to template in ESM build

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 // src/index.js
 import FormRAG from "./components/FormRAG.jsx";
 import { constructPrompt } from "./lib/promptEngine.js";
-
-// Use relative path that works from dist/
 import { UniversalSMBTemplate } from "../../templates/universal-smb.js";
 
 export { FormRAG, constructPrompt, UniversalSMBTemplate };
